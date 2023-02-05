c9a52dc49af51c21d2d1f8c30b0019f73c2e0912
Ilya Yalymov <chezeee@gmail.com>
2023-02-05 22:50:46 +0300

    Solution added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
-// define classes here
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  canVote() {
+    return this.age >= 18;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Admin extends User {
+  getFullName() {
+    return `${this.firstName} ${this.lastName} [admin]`;
+  }
+
+  updateConfig() {
+    return "Config updated";
+  }
+}
 
 // Sample usage - do not modify
 const user = new User("Sam", "Green", 17);
